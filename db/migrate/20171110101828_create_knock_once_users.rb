fb130f8637a78a5f378fe1f5bde482b96f6e5a01
Nicholas Shirley <nicholas@reallymy.email>
2017-11-12 09:07:45 +0100

    add user fields to migration and remove password table

--- db/migrate/20171110101828_create_knock_once_users.rb
+++ db/migrate/20171110101828_create_knock_once_users.rb
@@ -1,8 +1,18 @@
 class CreateKnockOnceUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :knock_once_users do |t|
+      # Required
+      t.string :email
+      t.string :password_digest
+      t.string :password_reset_token
+      t.datetime :password_token_expiry
+
+      # Add your custom user fields are required
+      # TODO add whitelist instructions here
 
       t.timestamps
     end
+
+    add_index :users, :email, unique: true
   end
 end
--- db/migrate/20171110102456_create_knock_once_passwords.rb
+++
@@ -1,8 +0,0 @@
-class CreateKnockOncePasswords < ActiveRecord::Migration[5.1]
-  def change
-    create_table :knock_once_passwords do |t|
-
-      t.timestamps
-    end
-  end
-end
