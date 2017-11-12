5f48fb55ecb37446d19160ea2247883358375899
Nicholas Shirley <nicholas@reallymy.email>
2017-11-12 18:44:50 +0100

    add DB migration

--- lib/generators/knock_once/install_generator.rb
+++ lib/generators/knock_once/install_generator.rb
@@ -2,12 +2,17 @@ module KnockOnce
   class InstallGenerator < Rails::Generators::Base
 
     source_root File.expand_path("../../../templates", __FILE__)
-    # def generate_routes
-
-    # end
 
     def create_user_model
       copy_file('user_model.rb', 'app/models/user.rb')
+    end
+
+    def copy_migrations
+      if self.class.migration_exists?("db/migrate", "create_knock_once_users")
+        say_status("skipped", "Migration create_knock_once_users already exists")
+      else
+        migration_template("create_knock_once_users.rb.erb", "db/migrate/create_knock_once_users.rb")
+      end
     end
   end
 end
