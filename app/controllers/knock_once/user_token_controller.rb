8611f42137d4b3d8894032ecf9aa5706e11e6138
Nicholas Shirley <nicholas@reallymy.email>
2017-11-13 12:15:35 +0100

    change path

--- app/controllers/knock_once/user_token_controller.rb
+++ app/controllers/knock_once/user_token_controller.rb
@@ -1,5 +1,5 @@
 
-require_dependency 'Knock/AuthTokenController'
+require_dependency 'Knock/auth_token_controller'
 
 module KnockOnce
   class UserTokenController < Knock::AuthTokenController
--- lib/knock_once/version.rb
+++ lib/knock_once/version.rb
@@ -1,3 +1,3 @@
 module KnockOnce
-  VERSION = '0.1.29'
+  VERSION = '0.1.300'
 end
