c1ae5330c9b52a0a7770a68517b6f1498a7a3ef0
Nicholas Shirley <nicholas@reallymy.email>
2017-11-14 17:21:45 +0100

    fix typo

--- app/controllers/knock_once/users_controller.rb
+++ app/controllers/knock_once/users_controller.rb
@@ -14,7 +14,7 @@ module KnockOnce
       @user = current_user
       if @user.authenticate(params[:current_password])
         # ActionController::Parameters.action_on_unpermitted_parameters = :raise
-        if @user.udpated(user_params).changes_applied
+        if @user.updated(user_params).changes_applied
           render json: {
             user: @user,
             message: 'Your profile has been updated!'
--- lib/knock_once/version.rb
+++ lib/knock_once/version.rb
@@ -1,3 +1,3 @@
 module KnockOnce
-  VERSION = '0.2.007'
+  VERSION = '0.2.008'
 end
