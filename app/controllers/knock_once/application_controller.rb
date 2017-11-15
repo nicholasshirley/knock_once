d78f0476724907bf54c4c66a57d0104461113e68
Nicholas Shirley <nicholas@reallymy.email>
2017-11-15 21:51:39 +0100

    move to app

--- app/controllers/knock_once/application_controller.rb
+++ app/controllers/knock_once/application_controller.rb
@@ -1,6 +1,5 @@
 module KnockOnce
   class ApplicationController < ActionController::API
     include Knock::Authenticable
-    require_dependency '../../models/knock_once/configuration'
   end
 end
--- lib/knock_once/version.rb
+++ lib/knock_once/version.rb
@@ -1,3 +1,3 @@
 module KnockOnce
-  VERSION = '0.2.059'
+  VERSION = '0.2.060'
 end
