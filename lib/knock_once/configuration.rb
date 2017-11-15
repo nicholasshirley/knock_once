84daa8012ab1fc5c33255870b9132853b4a56056
Nicholas Shirley <nicholas@reallymy.email>
2017-11-15 16:39:29 +0100

    change config blocks

--- lib/knock_once/configuration.rb
+++ lib/knock_once/configuration.rb
@@ -1,18 +1,21 @@
 module KnockOnce
-  class << self
-    attr_accessor :configuration
-  end
-
-  def self.configure
-    self.configuration ||= Configuration.new
-    yield(configuration)
-  end
-
   class Configuration
     attr_accessor :user_params
 
     def initialize
       @user_params = [:email, :current_password, :password, :password_confirmation]
     end
+  end
+
+  def self.configure
+    @configuration ||= Configuration.new
+  end
+
+  def self.configuration=(config)
+    @configuration = config
+  end
+
+  def self.configure
+    yield configuration
   end
 end
--- lib/knock_once/version.rb
+++ lib/knock_once/version.rb
@@ -1,3 +1,3 @@
 module KnockOnce
-  VERSION = '0.2.046'
+  VERSION = '0.2.047'
 end
