e8572cc1f992303b05adb4d5ad29840d9f170bed
Nicholas Shirley <nicholas@reallymy.email>
2017-11-15 16:57:37 +0100

    copy

--- lib/knock_once/configuration.rb
+++ lib/knock_once/configuration.rb
@@ -1,18 +1,37 @@
+# module KnockOnce
+#   class Configuration
+#     attr_accessor :user_params
+
+#     def initialize
+#       @user_params = [:email, :current_password, :password, :password_confirmation]
+#     end
+#   end
+
+#   class << self
+#     attr_accessor :configuration
+#   end
+
+#   def self.configure
+#     @configuration ||= Configuration.new
+#     yield configuration
+#   end
+# end
+
 module KnockOnce
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
+
   class Configuration
     attr_accessor :user_params
 
     def initialize
       @user_params = [:email, :current_password, :password, :password_confirmation]
     end
-  end
-
-  class << self
-    attr_accessor :configuration
-  end
-
-  def self.configure
-    @configuration ||= Configuration.new
-    yield configuration
   end
 end
--- lib/knock_once/version.rb
+++ lib/knock_once/version.rb
@@ -1,3 +1,3 @@
 module KnockOnce
-  VERSION = '0.2.048'
+  VERSION = '0.2.049'
 end
