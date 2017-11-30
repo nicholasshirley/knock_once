d2834ce6371060edc58bad3f6a7c55bce8c7cb57
Nicholas Shirley <nicholas@reallymy.email>
2017-11-30 10:27:17 +0100

    try refactor password model

--- app/controllers/knock_once/passwords_controller.rb
+++ app/controllers/knock_once/passwords_controller.rb
@@ -10,7 +10,10 @@ module KnockOnce
       # if valid user
       if @user
         # generate a new token and save
-        Password.reset_password(@user)
+        Password.new(@user)
+          .save_token_and_expiry
+          .email_reset
+
         render status: 200, json: {
           message: 'Your request has been received. If we have an email matching that account you will receive link to reset your password.'
         }
--- app/models/knock_once/password.rb
+++ app/models/knock_once/password.rb
@@ -1,24 +1,16 @@
 module KnockOnce
   class Password < ApplicationRecord
-    attr_accessor :token
-    attr_accessor :user
 
-    def self.reset_password (user)
-      save_token_and_expiry(user)
-      email_reset(user.email)
-    end
-
-    def self.generate_reset_token
+    def initialize(user)
       @token = SecureRandom.urlsafe_base64(KnockOnce.configuration.reset_token_length, false)
+      @user = user
     end
 
-    def self.save_token_and_expiry(user)
-      @user = user
-      generate_reset_token
+    def save_token_and_expiry
       User.find_by_email(@user['email']).update_attributes(password_reset_token: @token, password_token_expiry: KnockOnce.configuration.password_token_expiry)
     end
 
-    def self.email_reset(email)
+    def email_reset
       PasswordResetMailer.password_reset(@user, @token).deliver_now
     end
   end
